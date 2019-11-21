b1c1d113fa485545bb2b106f20f2d72c2ef07b68
Thomas Fish <rky95813@pw079.diamond.ac.uk>
2019-11-21 11:54:51 +0000

    Changed xrm files to output as '.ome.tif' by default, plus README update

--- src/run.py
+++ src/run.py
@@ -6,12 +6,19 @@ import sys
 
 def main(input, output=None):
     input_filepath = Path(input)
-    if output is None:
-        output_filepath = input_filepath
-        output_filepath.with_suffix(".ome.tiff")
-    else:
-        output_filepath = Path(output)
     if file_can_be_opened(input_filepath) and all_frames_written(input_filepath):
+
+        # If no output is supplied:
+        if output is None:
+            output_filepath = input_filepath
+            if input_filepath.suffix == ".txrm":
+                output_filepath.with_suffix(".ome.tiff")
+            elif input_filepath.suffix == ".xrm":
+                output_filepath.with_suffix(".ome.tif")
+
+        else:
+            output_filepath = Path(output)
+
         TxrmToTiff().convert(input_filepath, output_filepath)
 
 
