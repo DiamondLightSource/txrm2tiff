080f904040baceb4fbf89998e8feba22e940d127
Thomas Fish <thomas.fish@diamond.ac.uk>
2024-03-08 15:18:00 +0000

    update metadata creation tests

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,10 +1,8 @@
 from datetime import datetime
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock, patch
 
-import numpy as np
-
-from txrm2tiff.utils.metadata import create_ome_metadata
+from txrm2tiff.txrm import meta_mixin
 
 
 class TestMetadata(unittest.TestCase):
@@ -13,29 +11,45 @@ class TestMetadata(unittest.TestCase):
         dims = (45, 40, 1)
         mosaic_rows = 2
         mosaic_cols = 3
+        
+        with patch.object(meta_mixin.model, "Instrument") as mocked_instrument:
+            mocked_instrument.return_value = None
+            txrm = meta_mixin.MetaMixin()
+            txrm.strict = False
+            txrm.file_is_open = False
+            txrm.name = filename
+            txrm.output_shape = dims[::-1]
+            exposures = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0]
+            txrm.exposures = exposures
+            txrm.image_info = {
+                "PixelSize": [0.005],  # microns
+                "XPosition": [7.5, 22.5, 37.5] * 2,
+                "YPosition": [10, 10, 10, 20, 20, 20],
+                "ZPosition": [1, 1, 1, 1, 1, 1],
+            }
+            
+            txrm.energies = [500.] * len(exposures)
+            acquisition_time = datetime(2021, 12, 30, 23, 55, 59)
+            txrm.datetimes = [acquisition_time]
+            txrm.mosaic_dims = [mosaic_cols, mosaic_rows]
+            txrm.is_mosaic = False
 
-        txrm = MagicMock()
-
-        txrm.output_shape = dims[::-1]
-        exposures = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0]
-        txrm.exposures = exposures
-        txrm.image_info = {
-            "PixelSize": [0.005],
-            "XPosition": [7.5, 22.5, 37.5] * 2,
-            "YPosition": [10, 10, 10, 20, 20, 20],
-            "ZPosition": [1, 1, 1, 1, 1, 1],
-        }
-        txrm.datetimes = [datetime(2021, 12, 30, 23, 55, 59)]
-        txrm.mosaic_dims = [mosaic_cols, mosaic_rows]
-        txrm.is_mosaic = False
-
-        ome_metadata = create_ome_metadata(txrm, filename)
-        self.assertEqual(ome_metadata.image().Pixels.get_SizeX(), dims[0])
-        self.assertEqual(ome_metadata.image().Pixels.get_SizeY(), dims[1])
-        self.assertEqual(ome_metadata.image().Pixels.get_SizeT(), dims[2])
-        self.assertEqual(
-            ome_metadata.image().get_AcquisitionDate(), "2021-12-30T23:55:59"
-        )
+            pixel_size_nm = txrm.image_info["PixelSize"][0] * 1.e3
+            
+            print(txrm.metadata)
+            ome_metadata = txrm.metadata
+            self.assertEqual(ome_metadata.instruments, [])
+            self.assertIsNone(ome_metadata.images[0].instrument_ref)
+            self.assertEqual(ome_metadata.images[0].pixels.physical_size_x, pixel_size_nm)
+            self.assertEqual(ome_metadata.images[0].pixels.physical_size_y, pixel_size_nm)
+            self.assertEqual(ome_metadata.images[0].pixels.physical_size_z, 1)
+            self.assertEqual(ome_metadata.images[0].pixels.size_x, dims[0])
+            self.assertEqual(ome_metadata.images[0].pixels.size_y, dims[1])
+            self.assertEqual(ome_metadata.images[0].pixels.size_z, dims[2])
+            self.assertEqual(
+                ome_metadata.images[0].acquisition_date, acquisition_time
+            )
+            
 
     def test_mosaic_exposure_averaged(self):
         filename = "test_file.ext"
@@ -43,34 +57,37 @@ class TestMetadata(unittest.TestCase):
         mosaic_rows = 2
         mosaic_cols = 3
 
-        txrm = MagicMock()
+        with patch.object(meta_mixin.model, "Instrument") as mocked_instrument:
+            txrm = meta_mixin.MetaMixin()
+            txrm.strict = False
+            txrm.file_is_open = False
 
-        txrm.output_shape = dims[::-1]
-        exposures = [
-            2.0,
-            3.0,
-            4.0,
-            5.0,
-            0.0,
-            0.0,
-        ]  # 0 exposures should be ignored, if they exist, as these will be interrupted frames
-        txrm.exposures = exposures
-        txrm.image_info = {
-            "PixelSize": [0.005],
-            "XPosition": [7.5, 22.5, 37.5] * 2,
-            "YPosition": [10, 10, 10, 20, 20, 20],
-            "ZPosition": [1, 1, 1, 1, 1, 1],
-        }
-        txrm.datetimes = [datetime(2021, 12, 30, 23, 55, 59)]
-        txrm.mosaic_dims = [mosaic_cols, mosaic_rows]
-        txrm.is_mosaic = True
+            txrm.output_shape = dims[::-1]
+            exposures = [
+                2.0,
+                3.0,
+                4.0,
+                5.0,
+                0.0,
+                0.0,
+            ]  # 0 exposures should be ignored, if they exist, as these will be interrupted frames
+            txrm.exposures = exposures
+            txrm.image_info = {
+                "PixelSize": [0.005],
+                "XPosition": [7.5, 22.5, 37.5] * 2,
+                "YPosition": [10, 10, 10, 20, 20, 20],
+                "ZPosition": [1, 1, 1, 1, 1, 1],
+            }
+            txrm.datetimes = [datetime(2021, 12, 30, 23, 55, 59)]
+            txrm.mosaic_dims = [mosaic_cols, mosaic_rows]
+            txrm.is_mosaic = True
+            txrm.energies = [500.] * len(exposures)
 
-        expected_exposure = 3.5
+            expected_exposure = 3.5
 
-        ome_metadata = create_ome_metadata(txrm, filename)
-        self.assertEqual(
-            ome_metadata.image().Pixels.Plane(0).get_ExposureTime(), expected_exposure
-        )
+            self.assertEqual(
+                txrm.metadata.images[0].pixels.planes[0].exposure_time, expected_exposure
+            )
 
     def test_mosaic_centre_found_correctly(self):
         filename = "test_file.ext"
@@ -78,7 +95,7 @@ class TestMetadata(unittest.TestCase):
         pixel_size = 0.005
         mosaic_cols = 3
         mosaic_rows = 2
-        exposure_times = [0.0, 0.0, 2.0, 3.0, 4.0, 5.0]
+        exposures = [0.0, 0.0, 2.0, 3.0, 4.0, 5.0]
         offset = [3.0, -2.0]
         # Multiplier of 1.e3 required as units from xrm files are micrometres and the output should be in nanometres
         expected_centre = (
@@ -86,39 +103,43 @@ class TestMetadata(unittest.TestCase):
             (20.0 + offset[1]) * pixel_size * 1.0e3,
         )
 
-        txrm = MagicMock()
+        with patch.object(meta_mixin.model, "Instrument") as mocked_instrument:
+            txrm = meta_mixin.MetaMixin()
+            txrm.strict = False
+            txrm.file_is_open = False
 
-        txrm.output_shape = dims[::-1]
-        txrm.exposures = exposure_times
-        # This should only need the coords of the first frame as mosaic may not complete
-        txrm.image_info = {
-            "PixelSize": [pixel_size],
-            "XPosition": [
-                (7.5 + offset[0]) * pixel_size,
-                0,
-                0,
-                0,
-                0,
-                0,
-            ],
-            "YPosition": [
-                (10 + offset[1]) * pixel_size,
-                0,
-                0,
-                0,
-                0,
-                0,
-            ],
-            "ZPosition": [1, 1, 1, 1, 1, 1],
-        }
-        txrm.datetimes = [datetime(2021, 12, 30, 23, 55, 59)]
-        txrm.mosaic_dims = [mosaic_cols, mosaic_rows]
-        txrm.is_mosaic = True
+            txrm.output_shape = dims[::-1]
+            txrm.exposures = exposures
+            # This should only need the coords of the first frame as mosaic may not complete
+            txrm.image_info = {
+                "PixelSize": [pixel_size],
+                "XPosition": [
+                    (7.5 + offset[0]) * pixel_size,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                ],
+                "YPosition": [
+                    (10 + offset[1]) * pixel_size,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                ],
+                "ZPosition": [1, 1, 1, 1, 1, 1],
+            }
+            txrm.datetimes = [datetime(2021, 12, 30, 23, 55, 59)]
+            txrm.mosaic_dims = [mosaic_cols, mosaic_rows]
+            txrm.is_mosaic = True
+            txrm.energies = [500.] * len(exposures)
 
-        ome_metadata = create_ome_metadata(txrm, filename)
-        plane = ome_metadata.image().Pixels.Plane(0)
-        ome_centre = (float(plane.get_PositionX()), float(plane.get_PositionY()))
-        [
-            self.assertAlmostEqual(ome, expected)
-            for ome, expected in zip(ome_centre, expected_centre)
-        ]
+            ome_metadata = txrm.metadata
+            plane = ome_metadata.images[0].pixels.planes[0]
+            ome_centre = (float(plane.position_x), float(plane.position_y))
+            [
+                self.assertAlmostEqual(ome, expected)
+                for ome, expected in zip(ome_centre, expected_centre)
+            ]
