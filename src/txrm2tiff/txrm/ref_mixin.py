411aef5a092dac8bf8d6434320db39f405fb7818
yxd92326 <stephen.riggs@diamond.ac.uk>
2026-07-16 14:31:44 +0100

    External reference mixin takes fewer parameters

--- src/txrm2tiff/txrm/ref_mixin.py
+++ src/txrm2tiff/txrm/ref_mixin.py
@@ -109,8 +109,7 @@ class ReferenceMixin:
     ) -> np.ndarray:
         """Applies image(s) to txrm image(s), returning the images as a numpy ndarray and overwrites the txrm image(s) if overwrite is True."""
         self.apply_custom_reference_from_array(
-            txrm.get_images(load=True),
-            np.mean(txrm.exposures),
+            txrm.get_images(load=True)[0],
             compensate_exposure,
             overwrite,
         )
@@ -146,7 +145,7 @@ class ReferenceMixin:
             round(img_dim / mos_dim, 2)
             for img_dim, mos_dim in zip(self.shape, self.mosaic_dims[::-1])
         ]  # True if it's a mosaic and the correct dims to be tiled to mosaic
-        assert custom_reference.shape == self.shape or needs_stitching, (
+        assert list(custom_reference.shape) == self.shape or needs_stitching, (
             "Invalid reference shape for %s" % self.name
         )
         # Checks that reference is either the size of the image or can be stitched to that size
